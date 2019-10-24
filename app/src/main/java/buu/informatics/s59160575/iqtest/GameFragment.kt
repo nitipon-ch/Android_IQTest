b05f57679a925a0f8a2ad3081b99a175bff09200
Armakuji <draculalovekill@hotmail.com>
2019-10-25 01:10:29 +0700

    Add algorithm Successful Random & Compute

--- app/src/main/java/buu/informatics/s59160575/iqtest/GameFragment.kt
+++ app/src/main/java/buu/informatics/s59160575/iqtest/GameFragment.kt
@@ -2,12 +2,14 @@ package buu.informatics.s59160575.iqtest
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import buu.informatics.s59160575.iqtest.databinding.FragmentGameBinding
@@ -31,7 +33,7 @@ class GameFragment : Fragment() {
     lateinit var currentQuestion: Question
     lateinit var answers: MutableList<Int>
     private var questionIndex = 0
-    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
+    private var score = 0
 
     private lateinit var binding: FragmentGameBinding
     override fun onCreateView(
@@ -46,24 +48,60 @@ class GameFragment : Fragment() {
 
         binding.answerButton1.setOnClickListener { view ->
            if (questionIndex >= questions.size-1){
+               checkScore(0)
                view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
            }else{
+               checkScore(0)
                questionIndex++
                currentQuestion = questions[questionIndex]
                setQuestion()
+
                binding.invalidateAll()
+
            }
 
 
         }
         binding.answerButton2.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+            if (questionIndex >= questions.size-1){
+                checkScore(1)
+                view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+            }else{
+                checkScore(1)
+                questionIndex++
+                currentQuestion = questions[questionIndex]
+                setQuestion()
+
+                binding.invalidateAll()
+
+            }
         }
         binding.answerButton3.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+            if (questionIndex >= questions.size-1){
+                checkScore(2)
+                view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+            }else{
+                checkScore(2)
+                questionIndex++
+                currentQuestion = questions[questionIndex]
+                setQuestion()
+
+                binding.invalidateAll()
+
+            }
         }
         binding.answerButton4.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+            if (questionIndex >= questions.size-1){
+                checkScore(3)
+                view.findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+            }else{
+                checkScore(3)
+                questionIndex++
+                currentQuestion = questions[questionIndex]
+                setQuestion()
+                binding.invalidateAll()
+
+            }
         }
 
         return binding.root
@@ -77,6 +115,9 @@ class GameFragment : Fragment() {
 
     private fun setQuestion(){
         val imageQuestion : ImageView = binding.questionView
+        val textTitle : TextView = binding.questionText
+
+        textTitle.setText("Question ${questionIndex+1} / 12")
 
         currentQuestion = questions[questionIndex]
 
@@ -98,5 +139,11 @@ class GameFragment : Fragment() {
         ImageButton4.setBackgroundResource(answers[3])
     }
 
-
+    private fun checkScore(indexAns: Int) {
+        Log.i("GameFragment", "${answers[indexAns]}  :  ${currentQuestion.answers[0]}")
+        if(answers[indexAns] == currentQuestion.answers[0]){
+            score++
+        }
+        Log.i("GameFragment", "Score : ${score}")
+    }
 }
